e9f02ef2439c2d55c763c5a2520bce53106abe59
mateusCoder <mateus.moraes0507@gmail.com>
2022-08-11 14:45:56 -0300

    Created Project

---
+++ shop-style/src/main/java/com/mateus/ShopStyleApplication.java
@@ -0,0 +1,13 @@
+package com.mateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopStyleApplication.class, args);
+	}
+
+}
